6dacb6ef0888b5553f52de87942db7ae75611334
eduardo.saatkamp <eduardosaatkamp@gmail.com>
2024-10-24 05:01:42 -0300

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import React from 'react';
 import { useTranslation } from 'react-i18next';
 import ImcForm from './components/ImcForm/ImcForm';
 import ImcTable from './components/ImcTable/ImcTable'; 
 import GlucoseForm from './components/GlucoseForm/GlucoseForm';
 import GlucoseTable from './components/GlucoseTable/GlucoseTable';
 import Navbar from './components/TopBar/TopBar'; 
-import './App.css'; 
+import './App.css';
 
 const App = () => {
   const { i18n } = useTranslation();
